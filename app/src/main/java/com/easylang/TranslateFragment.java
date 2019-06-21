91b9a62292a38d96f66ee07bc9508b314f7c1cc8
Evgeniy Vasilyev <ju5tc4t@gmail.com>
2019-06-21 21:03:17 +0300

    Add translate fragment

---
+++ app/src/main/java/com/easylang/TranslateFragment.java
@@ -0,0 +1,34 @@
+package com.easylang;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+public class TranslateFragment extends Fragment {
+
+    private TranslateViewModel mViewModel;
+
+    public static TranslateFragment newInstance() {
+        return new TranslateFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_translate, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(TranslateViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/easylang/TranslateViewModel.java
@@ -0,0 +1,7 @@
+package com.easylang;
+
+import androidx.lifecycle.ViewModel;
+
+public class TranslateViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
