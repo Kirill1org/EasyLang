585eb02bde2640445b62ef7840d6d35d82002976
Evgeniy Vasilyev <ju5tc4t@gmail.com>
2019-06-21 22:29:36 +0300

    Add dictionary fragment

---
+++ app/src/main/java/com/easylang/DictionaryFragment.java
@@ -0,0 +1,34 @@
+package com.easylang;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+public class DictionaryFragment extends Fragment {
+
+    private DictionaryViewModel mViewModel;
+
+    public static DictionaryFragment newInstance() {
+        return new DictionaryFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dictionary, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(DictionaryViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
